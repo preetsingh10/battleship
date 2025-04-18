6a9cca8c650cafb227a50de768b10768e4f7ccfb
Preet Singh <preet10syd@gmail.com>
2025-04-19 06:44:14 +1000

    edited some test and logic starts to make sense :)

--- tests/gameBoard.test.js
+++ tests/gameBoard.test.js
@@ -1,15 +1,34 @@
-// import Gameboard from "../gameBoard";
-// import Ship from "../ship";
-const Ship = require("../ship");
 const Gameboard = require("../Gameboard");
+
+const gameboard = new Gameboard();
+gameboard.placeShip(gameboard.carrier, "horizontal", [1, 1]);
+gameboard.placeShip(gameboard.battleship, "vertical", [7, 7]);
 
 describe("Gameboard - receiveAttack", () => {
   test("attacking a ship", () => {
-    const gameboard = new Gameboard();
-    const ship = new Ship(5);
-    gameboard.placeShip(ship, "horizontal", [1, 1]);
     expect(gameboard.receiveAttack(1, 1)).toBe(true);
     expect(gameboard.receiveAttack(2, 1)).toBe(true);
     expect(gameboard.receiveAttack(2, 7)).toBe(false);
+    expect(gameboard.receiveAttack(5, 5)).toBe(false);
+  });
+});
+
+describe("keep track of the missed attacks", () => {
+  test("recording missed attacks ", () => {
+    expect(gameboard.missedAttacks).toEqual([
+      [2, 7],
+      [5, 5],
+    ]);
+  });
+});
+
+describe("Ship placement", () => {
+  test("testing ship placement", () => {
+    expect(gameboard.battleship.position).toEqual([
+      [7, 7],
+      [7, 8],
+      [7, 9],
+      [7, 10],
+    ]);
   });
 });
